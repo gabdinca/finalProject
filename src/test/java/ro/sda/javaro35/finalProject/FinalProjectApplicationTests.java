727040d2f7567a7e9c588c4139cad0b889e6e43b
gabdinca <gabriel.dinca@protonmail.com>
2022-07-19 18:23:58 +0300

    Init Spring project

---
+++ src/test/java/ro/sda/javaro35/finalProject/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.sda.javaro35.finalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
