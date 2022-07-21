e5361f88e8ce815fe37a342a50370e243a52e42b
imarc <marcu_iulian14@yahoo.com>
2022-07-21 22:10:00 +0300

    Add - enum, RecipeRepository,Ingredient, IngredientValidator

--- src/main/java/ro/sda/javaro35/finalProject/entities/User.java
+++ src/main/java/ro/sda/javaro35/finalProject/entities/User.java
@@ -4,106 +4,28 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import ro.sda.javaro35.finalProject.enums.UserRole;
 
-import javax.persistence.*;
-
-import java.util.Collection;
-import java.util.Collections;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import static javax.persistence.GenerationType.AUTO;
 import static lombok.AccessLevel.PRIVATE;
 
-
 @Entity(name = "user")
 @NoArgsConstructor
 @Data
-@NoArgsConstructor
 @FieldDefaults(level = PRIVATE)
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = AUTO)
     Long id;
-    @Column(nullable = false)
     String userName;
-    @Column(nullable = false)
     String password;
-    @Column(nullable = false)
     String firstName;
-    @Column(nullable = false)
     String lastName;
-    @Column(unique = true, nullable = false)
     String email;
     Integer age;
-    @Enumerated(EnumType.STRING)
-    UserRole userRole;
-    Boolean locked = false;
-    Boolean enabled = false;
-
-    public User(String firstname, String lastname, String email, String password, Integer age, UserRole userRole) {
-
-        this.password = password;
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.email = email;
-        this.age = age;
-        this.userRole = userRole;
-
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(userRole.name());
-        return Collections.singletonList(authority);
-    }
-
-    @Override
-    public String getPassword(){
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-
-    public String getFirstName() {
-        return firstname;
-    }
-
-    public String getLastName() {
-        return lastname;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstname = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastname = lastName;
-    }
 
 }
