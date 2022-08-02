42a90f3e7bb0355de342b0e51b297d3750fafc8f
imarc <marcu_iulian14@yahoo.com>
2022-08-02 21:49:46 +0300

    add background image & nav bar in homepage

--- src/main/java/ro/sda/javaro35/finalProject/controller/RecipeController.java
+++ src/main/java/ro/sda/javaro35/finalProject/controller/RecipeController.java
@@ -1,24 +1,33 @@
 package ro.sda.javaro35.finalProject.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ro.sda.javaro35.finalProject.dto.RecipeDto;
-import ro.sda.javaro35.finalProject.entities.Recipe;
 import ro.sda.javaro35.finalProject.services.RecipeService;
 
-@RestController
+import java.util.List;
+
+@Controller
 @RequestMapping(path = "/recipe")
 @AllArgsConstructor
 public class RecipeController {
     private final RecipeService recipeService;
 
+    @GetMapping()
+    public String getRecipeById() {
+        // TODO: IMPLEMENT METHOD GET recipe
+        return "allRecipe";
+    }
+
     @Transactional
     @PostMapping
-    public String createRecipe(@RequestBody RecipeDto recipe) {
+    public String createRecipe(@ModelAttribute RecipeDto recipe) {
         return recipeService.createRecipe(recipe);
     }
 }
