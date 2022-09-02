2de88acb8f91416c15758d9705da5520d7098834
gabdinca <gabriel.dinca@protonmail.com>
2022-09-02 16:20:31 +0300

    Add findRecipesByIngredientsIn in RecipeRepository

--- src/main/java/ro/sda/javaro35/finalProject/repository/RecipeRepository.java
+++ src/main/java/ro/sda/javaro35/finalProject/repository/RecipeRepository.java
@@ -1,10 +1,6 @@
 package ro.sda.javaro35.finalProject.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import ro.sda.javaro35.finalProject.entities.Ingredient;
 import ro.sda.javaro35.finalProject.entities.Recipe;
 
